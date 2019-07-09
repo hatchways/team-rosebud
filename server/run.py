265b2d366118f5ca4f5ebcbd52897c918ccbd1ec
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-07-09 15:22:20 -0400

    Modified category resource
    Includes post, put, delete
    Verified db is accepting these commands
    Validation and serialization taken care by Marshmallow as recommended by Flask-Restful documenation (over reqparse)

--- server/run.py
+++ server/run.py
@@ -8,12 +8,6 @@ def create_app(config_filename):
     from app import api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
 
-    from api.ping_handler import ping_handler
-    app.register_blueprint(ping_handler)
-
-    from api.home_handler import home_handler
-    app.register_blueprint(home_handler)
-
     from models import db
     db.init_app(app)
 
