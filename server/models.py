aa25f8709b1b42b9b1a813c2a9698756992e3110
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-07-11 16:32:23 -0400

    Added Project Model
    Model migrated and displaying on db. Moving on to creating Project resource

--- server/models.py
+++ server/models.py
@@ -51,3 +51,33 @@ class Category(db.Model):
 class CategorySchema(ma.Schema):
     id = fields.Integer()
     name = fields.String(required=True)
+
+class ProjectModel(db.Model):
+    __tablename__ = 'projects'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False, unique=True)
+    link = db.Column(db.String(120), nullable=False, unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    @classmethod
+    def find_by_name(cls, name: str) -> "ProjectModel":
+        return cls.query.filter_by(name=name).first()
+
+    @classmethod
+    def find_by_id(cls, _id: int) -> "ProjectModel":
+        return cls.query.filter_by(id=_id).first()
+
+    def save_to_db(self) -> None:
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self) -> None:
+        db.session.delete(self)
+        db.session.commit()
+
+class ProjectSchema(ma.ModelSchema):
+    
+    class Meta:
+        model = ProjectModel
+    user = ma.Nested(UserSchema)
