334b9634400d5eac50125a4f695a83b7ebb580aa
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-07-19 16:23:59 -0400

    added some changes to project model and resource
    resource + endpoint modified

--- server/app.py
+++ server/app.py
@@ -15,8 +15,9 @@ api.add_resource(UserLogin, "/login")
 api.add_resource(UserSkill, "/user/<int:user_id>/skill")
 api.add_resource(TokenRefresh, "/refresh")
 api.add_resource(UserLogout, "/logout")
-api.add_resource(ProjectCreate, "/create")
+api.add_resource(ProjectCreate, "/user/<int:user_id>/project")
 api.add_resource(Project, "/project/<int:project_id>")
-api.add_resource(SkillCreate, "/skill")
+api.add_resource(SkillCreate, "/skill") #to remove
 api.add_resource(Skill, "/skill/<int:skill_id>")
 api.add_resource(SkillList, "/skills")
+#to add userlist for global view
