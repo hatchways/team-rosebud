0cde2e973edfe0bd470404e2ba9e75da617b5c83
Sanchit Jain <sanchit.jain@mail.mcgill.ca>
2019-07-24 11:10:51 -0400

    Added image uploading functionality
    added boto3 and set up aws

--- server/resources/Image.py
+++ server/resources/Image.py
@@ -1,3 +1,8 @@
 from cStringIO import cStringIO
 
-from boto.s3.connection 
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key as S3Key
+from flask_restful import Resource, reqparse, abort
+from flask import request
+from werkzeug.datastructures import FileStorage
+
